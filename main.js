660a2321ea825db3bd7bd09708240b54bd9d4dee
ankit <96295668+AnkitRajavat@users.noreply.github.com>
2022-03-09 19:28:23 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,69 @@
+/*// document.write("this is document type");
+//         // console.log("hello world");
+//         console.warn("this is warning");
+//         console.error("this is error");
+var num1=24;
+var num2=45;
+console.log(num1+num2);
+var str='this is string ';
+var str2="this is also string ";
+console.log(str+str2);
+var  a=true;
+var b=false;
+var d;
+var c=undefined;
+var e=null;
+console.log(c);
+console.log(e);
+var mark={
+ a: "ankit",
+ b: 'kumar',   
+ c: 'rajavat',
+ d: 9870
+}
+ console.log(mark);
+ var arr=[7,5,"thisf","yrr",4];
+ console.log(arr);
+ var num=5;
+ if(num>4){
+     console.log("this is ankit");
+
+ }
+let ank=["this",4,52,67,4,8,"jkaskdf"];
+
+
+
+//ank.unshift("wake up");
+ank.push(ank.length)
+console.log(ank);
+//ank.shift();
+ank.splice(4);
+console.log(ank);
+
+let ank="now timing is 4:50";
+console.log(ank.length)
+console.log(ank.indexOf("is"));
+console.log(ank.slice(0,5))
+let date =new Date();
+console.log(date);
+console.log(date.getTime());
+console.log(date.getFullYear())
+
+console.log(date.getMonth())
+
+console.log(date.getDay())*/
+// document.getElementById(click);
+
+// sel=document.querySelectorAll(".container");
+// console.log(sel);
+
+logkaro=() =>{
+    console.log(" i am your log");
+
+}
+setTimeout(logkaro ,2000);
+//setInterval(logkaro ,5000);
+let co=["this",]
+clr=new json();
+
+console.log(clr);
